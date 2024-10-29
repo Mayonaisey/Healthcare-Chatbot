1e4270ff2e6ffd12e62c2a941427b2a207ed0c61
zaha hany <zahahani247@gmail.com>
2024-10-30 00:51:10 +0300

    .

--- appointments.php
+++ appointments.php
@@ -31,42 +31,10 @@
   
   <!--el header file henaaaa-->
   <?php include 'header.html';?>
-  
+
   <div class="d-flex align-items-stretch">
-    <!-- Sidebar Navigation-->
-    <nav id="sidebar">
-      <!-- Sidebar Header-->
-      <div class="sidebar-header d-flex align-items-center">
-        <div class="avatar"><img src="img/avatar-6.jpg" alt="..." class="img-fluid rounded-circle"></div>
-        <div class="title">
-          <h1 class="h5">Mark Stephen</h1>
-          <p>Web Designer</p>
-        </div>
-      </div>
-      <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
-      <ul class="list-unstyled">
-        <li><a href="index.html"> <i class="icon-home"></i>Home </a></li>
-        <li><a href="tables.php"> <i class="icon-grid"></i>Tables </a></li>
-        <li><a href="charts.html"> <i class="fa fa-bar-chart"></i>Charts </a></li>
-        <li class="active"><a href="forms.html"> <i class="icon-padnote"></i>Appointments </a></li>
-        <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i
-              class="icon-windows"></i>Example dropdown </a>
-          <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
-            <li><a href="#">Page</a></li>
-            <li><a href="#">Page</a></li>
-            <li><a href="#">Page</a></li>
-          </ul>
-        </li>
-        <li><a href="login.html"> <i class="icon-logout"></i>Login page </a></li>
-    </nav>
-    <!-- Sidebar Navigation end-->
-    <div class="page-content">
-      <!-- Page Header-->
-      <div class="page-header no-margin-bottom">
-        <div class="container-fluid">
-          <h2 class="h5 no-margin-bottom">Appointments</h2>
-        </div>
-      </div>
+  <?php include 'side.html';?>
+
       <?php
       // Database connection credentials
       $servername = "localhost";
